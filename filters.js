7571cd9acec354a6862b6a47d11cd6b10ee108e8
liyang <1466977749@qq.com>
2019-04-19 10:37:03 +0800

    增加一些过滤器

--- filters.js
+++ filters.js
@@ -8,13 +8,13 @@ exports.stringFiltering = (value) =>{
         return '';
     }
 };
-// 如果小数位数超过7位，则按科学计数法表示
-exports.science = (value) => {
-    if(value.toString().indexOf(".") == -1||value.toString().split(".")[1].length > 7 ){
-        value=value.toExponential(2);
-        return value
-    }else {
-        return value;
+
+// 保留几位洗小数带符号
+exports.formatNum2 = (value) =>{
+    if(value > 0){
+        return "+"+value.toFixed(2);
+    }else{
+        return value.toFixed(2);
     }
 };
 // 保留几位洗小数
@@ -25,30 +25,57 @@ exports.formatNum = (value,data) =>{
         return '';
     }
 };
-// 保留几位洗小数带符号
-exports.formatNum2 = (value) =>{
-    if(value > 0){
-        return "+"+value.toFixed(2);
-    }else{
-        return value.toFixed(2);
+// 格式化日期
+exports.formatDate = (value,type)=>{
+    let year =value.split(" ")[0].split("-")[0];
+    let mou =value.split(" ")[0].split("-")[1];
+    let day =value.split(" ")[0].split("-")[2];
+    let hour=value.split(" ")[1].split(":")[0];
+    let min=value.split(" ")[1].split(":")[1];
+    let sec=value.split(" ")[1].split(":")[2];
+    if(type==0){
+        return mou+'/'+day;
+    }else if(type==1){
+        return hour+':'+min;
+    }else if(type ==2){
+        return year+'-'+mou+'-'+day;
     }
 };
+// 时间戳转换成日期
+exports.formatDate1 = (value)=>{
+    var time = new Date(value)    //先将时间戳转为Date对象，然后才能使用Date的方法
+    var year = time.getFullYear(),
+        month = time.getMonth() + 1 ,  //月份是从0开始的
+        day = time.getDate(),
+        hour = time.getHours(),
+        minute = time.getMinutes(),
+        second = time.getSeconds()
+    //add0()方法在后面定义
+    return  year+'-'+add0(month)+'-'+ add0(day)+'  '+add0(hour)+':'+add0(minute)+':'+add0(second)
 
-
-// 时间戳格式化成日期
-exports.formatDate =(val) =>{
-    const date = new Date(val);
-    const year = date.getFullYear();
-    const month = date.getMonth() > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-    const day = date.getDate() > 9 ? date.getDate() + 1 : `0${date.getDate() + 1}`;
-    return `${year}-${month}-${day}`;
 };
 
-// 时间戳格式化成时分秒
-exports.formatTime =(val) =>{
-    const date = new Date(val);
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return `${hour} : ${minutes} : ${seconds}`;
+exports.formatNum7 = (value,data) =>{
+    if(value.toString().split(".")[1] && value.toString().split(".")[1].length > 7){
+        return value.toFixed(data);
+    } else{
+        return value;
+    }
 };
+//超过6位数的小数用科学计数法表示
+exports.science = (value) => {
+    if(value.toString().indexOf(".") == -1||value.toString().split(".")[1].length > 7 ){
+        value=value.toExponential(2);
+        return value
+    }else {
+        return value;
+    }
+};
+//时间转换成时间戳
+exports.formatDate2=(value)=>{
+    console.log(value);
+    let date = new Date(value).getTime();
+    let nowDate=new Date().getTime();
+    return Math.ceil((nowDate-date)/(4*3600000));
+};
+
